a0db96e207ad462f528e4ea02267895504960c20
Mateus Aubin <mateus.aubin+git@gmail.com>
2014-04-26 10:19:30 -0300

    solution

--- sharpsnmplib-master/lib/SharedAssemblyInfo.cs
+++ sharpsnmplib-master/lib/SharedAssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Reflection;
 // You can specify all the values or you can use the default the Revision and
 // Build Numbers by using the '*' as shown below:
 
-[assembly: AssemblyVersion("7.6.010426.04")]
+[assembly: AssemblyVersion("7.6.010426.07")]
 #if (!CF)
-[assembly: AssemblyFileVersion("7.6.010426.04")]
+[assembly: AssemblyFileVersion("7.6.010426.07")]
 #endif
